3ebedb92eedfd925837001808f1e70f91d4a12c5
Nikita <nikita.ableev@mail.ru>
2021-05-28 19:27:54 +0300

    Update slider

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import "Styles/_app.scss";
+
+import Swiper from "swiper";
+
+// slide
+{
+  $(() => {
+    const slider = $('.slider');
+
+    if (slider.length !== 0 ) {
+      const swiper = new Swiper(slider[0] , {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+      });
+    }
+  });
+}
