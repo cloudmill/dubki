b5c6f30a44578586457748e600e06217d4bb8c51
Nikita <nikita.ableev@mail.ru>
2021-05-31 16:26:27 +0300

    Update top

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import "Styles/_app.scss";
 
 import Swiper from "swiper";
 
-// slide
+// slider
 {
   $(() => {
     const slider = $('.slider');
@@ -12,6 +12,33 @@ import Swiper from "swiper";
         slidesPerView: 'auto',
         spaceBetween: 20,
       });
+    }
+  });
+}
+
+// top slider
+{
+  $(() => {
+    const topSlider = $('.top__container');
+
+    if (topSlider.length !== 0) {
+      const swiper = new Swiper(topSlider[0] , {
+        // watchSlidesProgress: true,
+        // watchSlidesVisibility: 3,
+        // slideVisibleClass: 'top-slide--visible',
+        slidesPerView: 'auto',
+        slidesPerGroup: 3,
+        spaceBetween: 25,
+        // effect: 'flip',
+        // flipEffect: {
+        //   slideShadows: true,
+        // }
+
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+      })
     }
   });
 }
