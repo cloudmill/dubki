bdfd01fc6ea5a4e8d634b020a2f3b614be1d1040
Gercin <rockinpeople.rip@gmail.com>
2021-06-22 16:37:12 +0300

    update backend

--- src/assets/scripts/backend.js
+++ src/assets/scripts/backend.js
@@ -6,18 +6,17 @@ $(() => {
 $(function () {
   vacancy();
   submitForm();
-  redirectBuyList();
+  redirectBuy();
+  recipes();
 });
 
 function vacancy() {
-  console.log("vacancy");
   $("[data-type=js-vacancy-filter]").on("change", function (e) {
     ajaxVacancy(this.value);
   });
 }
 
 function ajaxVacancy(cityId) {
-  let tags = [],
     vacancyList = $("[data-type=js-vacancy-list]");
 
   $.ajax({
@@ -29,6 +28,39 @@ function ajaxVacancy(cityId) {
     },
   }).done(function (a) {
     vacancyList.html(a);
+  });
+}
+
+function recipes() {
+  console.log("recipes");
+  $("[data-type=js-recipes-filter-type]").on("change", function (e) {
+    console.log("change type");
+    time = $("[data-type=js-recipes-filter-time]").value;
+    ajaxRecipes(this.value, time);
+  });
+  $("[data-type=js-recipes-filter-time]").on("change", function (e) {
+    console.log("change time");
+    type = $("[data-type=js-recipes-filter-type]").value;
+    ajaxRecipes(type, this.value);
+  });
+}
+
+function ajaxRecipes(type, time) {
+  console.log("ajax Recipes");
+  console.log(type);
+  console.log(time);
+  recipesList = $("[data-type=js-recipes-list]");
+
+  $.ajax({
+    method: "POST",
+    url: window.location.href,
+    data: {
+      ajax: 1,
+      time: time,
+      type: type
+    },
+  }).done(function (a) {
+    recipesList.html(a);
   });
 }
 
@@ -63,8 +95,12 @@ function submitForm() {
   });
 }
 
-function redirectBuyList() {
-  $("[data-type=redirect-buy-list]").on("click", function () {
-    window.location.href = "/buy/list";
+function redirectBuy() {
+  $('[data-type=redirect-buy-list]').on('click', function() {
+    window.location.href = '/buy/list';
+  });
+
+  $('[data-type=redirect-buy-map]').on('click', function() {
+    window.location.href = '/buy';
   });
 }
